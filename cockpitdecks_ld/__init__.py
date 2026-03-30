98f5204be4b95491f540a55f1256415a4c233960
dlicudi <dlicudi@gmail.com>
2026-03-30 18:15:10 +0200

    chore: bump version to 1.2.3

--- cockpitdecks_ld/__init__.py
+++ cockpitdecks_ld/__init__.py
@@ -7,4 +7,4 @@ from datetime import datetime
 __NAME__ = "cockpitdecks_ld"
 __COPYRIGHT__ = f"© 2022-{datetime.now().strftime('%Y')} Pierre M <pierre@devleaks.be>"
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
